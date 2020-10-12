6747b9227f7f1edf618c730efacd4d880628881c
Yuanhao Zhuo <153376251@qq.com>
2020-10-12 09:05:49 +0800

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,5 @@
 """ 这是唯一一个不是ALBERT的部分 这部分的目的是从MongoDB中读取数据，并存成.CSV文件以供后续使用
-目标文件分两列，第一列content（中文文本），第二列label（0-24数值）
+目标文件分两列，第一列content（中文文本），第二列label（0-24数值）(需要与其他程序中定义的cat类型对应)
 MongoDB中的数据处理程序和来源的代码恕不提供
 Since in this project, the original data is stored in MongoDB, this part is used to save the data in MongoDB as CSV
 The target file is divided into two columns, the first column is content (Chinese text), the second column is label (0-24 value)
@@ -20,7 +20,7 @@ db_des_training = db_des[TRAININGTABLE]
 db_des_val = db_des[VALTABLE]
 
 #首先处理train
-search=db_des_training.find(no_cursor_timeout = True)#no_cursor_timeout = True不加的话MongoDB会超时
+search=db_des_training.find(no_cursor_timeout = True)#########no_cursor_timeout = True：数据量较大时，不加的话MongoDB会超时，pymongo默认是10分钟超时
 i=0
 data=[]
 data.append(("content","label"))
@@ -34,7 +34,8 @@ for record in search:
     i=i+1
     if i%1000==0:
         print("------finished loading train set #",i)
-f = codecs.open('.../sa_train.csv','w','utf-8')#这个地址要改！！！！
+f = codecs.open('.../sa_train.csv','w','utf-8')##############这个地址要改！！！！
+#####注意文件名是sa_train.csv！！！！
 writer = csv.writer(f)
 for i in data:
     writer.writerow(i)
@@ -42,7 +43,7 @@ f.close()
 
 
 #然后处理val
-search=db_des_val.find(no_cursor_timeout = True)#no_cursor_timeout = True不加的话MongoDB会超时
+search=db_des_val.find(no_cursor_timeout = True)#######no_cursor_timeout = True不加的话MongoDB会超时
 i=0
 data=[]
 data.append(("content","label"))
@@ -57,7 +58,9 @@ for record in search:
     if i%1000==0:
         print("------finished loading val set #",i)
 f = codecs.open('.../sa_test.csv','w','utf-8')#这个地址要改！！！！
+#####注意文件名是sa_test.csv！！！！
 writer = csv.writer(f)
 for i in data:
     writer.writerow(i)
 f.close()
+#有需要test集的话再加一段即可
